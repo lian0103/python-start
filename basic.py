56bd08ac602ac6414055ad40f49a8d910a1bd8ae
lian0103 <k1319900103@gmail.com>
2022-08-01 21:45:18 +0800

    start

---
+++ basic.py
@@ -0,0 +1,14 @@
+# print
+print('hello')
+
+# data type
+data_str = 'hello world'
+data_bool = True
+data_num = 123
+data_float = 12.5
+
+# data type format
+data_num_to_str = str(data_num)  # '123'
+data_float_to_int = int(data_float)  # 12
+
+
